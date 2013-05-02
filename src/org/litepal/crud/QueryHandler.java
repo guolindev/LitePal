2661b4e240a8f30be0da46803306bad4cc9ae537
Tony Green <sinyu890807@126.com>
2013-05-02 21:27:16 +0800

    Commit a copy of current progress. In case losing data of upgrading system.

--- src/org/litepal/crud/QueryHandler.java
+++ src/org/litepal/crud/QueryHandler.java
@@ -1,6 +1,9 @@
 package org.litepal.crud;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 
 import org.litepal.util.BaseUtility;
@@ -16,29 +19,31 @@ class QueryHandler extends DataHandler {
 	}
 
 	<T> T onFind(Class<T> modelClass, long id) {
-		List<Field> supportedFields = getSupportedFields(modelClass.getName());
-		String tableName = getTableName(modelClass);
-		Cursor cursor = mDatabase.query(tableName, null, "id = " + id, null, null, null, null);
-		if (cursor.moveToFirst()) {
-			for (Field field : supportedFields) {
-				String methodName = genGetColumnMethod(field);
-				LogUtil.d(TAG, "method name is " + methodName);
-				int columnIndex = cursor.getColumnIndex(BaseUtility.changeCase(field.getName()));
-				LogUtil.d(TAG, "field name is " + field.getName() + " column index is "
-						+ columnIndex);
-				if (columnIndex != -1) {
-					Class<?> cursorClass = cursor.getClass();
-					try {
-						cursorClass.getMethod(methodName, int.class);
-					} catch (SecurityException e) {
-						e.printStackTrace();
-					} catch (NoSuchMethodException e) {
-						e.printStackTrace();
+		try {
+			Constructor<?> constructor = findBestSuitConstructor(modelClass);
+			T con = (T)constructor.newInstance(getConstructorParams(constructor));
+			List<Field> supportedFields = getSupportedFields(modelClass.getName());
+			String tableName = getTableName(modelClass);
+			Cursor cursor = mDatabase.query(tableName, null, "id = " + id, null, null, null, null);
+			if (cursor.moveToFirst()) {
+				for (Field field : supportedFields) {
+					String methodName = genGetColumnMethod(field);
+					LogUtil.d(TAG, "method name is " + methodName);
+					int columnIndex = cursor.getColumnIndex(BaseUtility.changeCase(field.getName()));
+					LogUtil.d(TAG, "field name is " + field.getName() + " column index is "
+							+ columnIndex);
+					if (columnIndex != -1) {
+						Class<?> cursorClass = cursor.getClass();
+						Method method = cursorClass.getMethod(methodName, int.class);
+						Object object = method.invoke(cursor, columnIndex);
+						LogUtil.d(TAG, "object is " + object);
 					}
 				}
 			}
+			cursor.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		cursor.close();
 		return null;
 	}
 
