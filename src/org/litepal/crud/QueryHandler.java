c1d7ad9c40f0414665e159848e8c2578d8546ff8
Tony Green <sinyu890807@126.com>
2013-05-24 22:36:35 +0800

    Add support query object containing char type.

--- src/org/litepal/crud/QueryHandler.java
+++ src/org/litepal/crud/QueryHandler.java
@@ -2,7 +2,6 @@ package org.litepal.crud;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
 
@@ -21,7 +20,8 @@ class QueryHandler extends DataHandler {
 	<T> T onFind(Class<T> modelClass, long id) {
 		try {
 			Constructor<?> constructor = findBestSuitConstructor(modelClass);
-			T con = (T)constructor.newInstance(getConstructorParams(constructor));
+			DataSupport modelInstance = (DataSupport) constructor
+					.newInstance(getConstructorParams(constructor));
 			List<Field> supportedFields = getSupportedFields(modelClass.getName());
 			String tableName = getTableName(modelClass);
 			Cursor cursor = mDatabase.query(tableName, null, "id = " + id, null, null, null, null);
@@ -29,18 +29,32 @@ class QueryHandler extends DataHandler {
 				for (Field field : supportedFields) {
 					String methodName = genGetColumnMethod(field);
 					LogUtil.d(TAG, "method name is " + methodName);
-					int columnIndex = cursor.getColumnIndex(BaseUtility.changeCase(field.getName()));
+					int columnIndex = cursor
+							.getColumnIndex(BaseUtility.changeCase(field.getName()));
 					LogUtil.d(TAG, "field name is " + field.getName() + " column index is "
 							+ columnIndex);
 					if (columnIndex != -1) {
 						Class<?> cursorClass = cursor.getClass();
 						Method method = cursorClass.getMethod(methodName, int.class);
-						Object object = method.invoke(cursor, columnIndex);
-						LogUtil.d(TAG, "object is " + object);
+						Object value = method.invoke(cursor, columnIndex);
+						LogUtil.d(TAG, "value is " + value);
+						LogUtil.d(TAG, "type is " + field.getType());
+						if (field.getType() == boolean.class || field.getType() == Boolean.class) {
+							if ("0".equals(String.valueOf(value))) {
+								value = false;
+							} else if ("1".equals(String.valueOf(value))) {
+								value = true;
+							}
+						} else if (field.getType() == char.class
+								|| field.getType() == Character.class) {
+							value = ((String) value).charAt(0);
+						}
+						putSetMethodValueByField(modelInstance, field, value);
 					}
 				}
 			}
 			cursor.close();
+			return (T) modelInstance;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -48,13 +62,19 @@ class QueryHandler extends DataHandler {
 	}
 
 	private String genGetColumnMethod(Field field) {
-		String fieldType;
-		if (field.getType().isPrimitive()) {
-			fieldType = BaseUtility.capitalize(field.getType().getName());
+		String typeName;
+		Class<?> fieldType = field.getType();
+		if (fieldType.isPrimitive()) {
+			typeName = BaseUtility.capitalize(fieldType.getName());
 		} else {
-			fieldType = field.getType().getSimpleName();
+			typeName = fieldType.getSimpleName();
 		}
-		String methodName = "get" + fieldType;
+		String methodName = "get" + typeName;
+		if ("getBoolean".equals(methodName)) {
+			methodName = "getInt";
+		} else if ("getChar".equals(methodName)) {
+			methodName = "getString";
+		}
 		return methodName;
 	}
 
