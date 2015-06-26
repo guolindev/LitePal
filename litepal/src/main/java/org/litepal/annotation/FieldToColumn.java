fb8ce15c2e931cbea174bbaf4ba98edcbf8dc9f7
Tony Green <sinyu890807@126.com>
2015-06-26 11:24:59 +0800

    Add custom annotations. Used for targeting a specified table name and column name.

---
+++ litepal/src/main/java/org/litepal/annotation/FieldToColumn.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C)  Tony Green, Litepal Framework Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.litepal.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Used for adding constraints to a column. Besides, you can targeting a specified
+ * column name instead of the default one. It's not recommended to use unless you
+ * had a legacy table. The default column name created by LitePal should always be
+ * your first choice.
+ *
+ * @author Tony Green
+ * @since 1.3
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface FieldToColumn {
+    String name() default "";
+    boolean nullable() default true;
+    boolean unique() default false;
+}
