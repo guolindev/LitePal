5bb0d98ce612f3eafcc98483d7c7f0dc5790b4d2
Tony Green <sinyu890807@126.com>
2018-01-25 10:48:50 +0800

    Fix bug https://github.com/LitePalFramework/LitePal/issues/322

--- litepal/src/main/java/org/litepal/crud/DataSupport.java
+++ litepal/src/main/java/org/litepal/crud/DataSupport.java
@@ -106,7 +106,7 @@ public class DataSupport {
 	/**
 	 * A map contains all the associated models' id with M2M association.
 	 */
-	private Map<String, Set<Long>> associatedModelsMapForJoinTable;
+	private Map<String, List<Long>> associatedModelsMapForJoinTable;
 
 	/**
 	 * When updating a model and the associations breaks between current model
@@ -2142,10 +2142,10 @@ public class DataSupport {
 	 *            The id of associated model.
 	 */
 	void addAssociatedModelForJoinTable(String associatedModelName, long associatedId) {
-		Set<Long> associatedIdsM2MSet = getAssociatedModelsMapForJoinTable().get(
+		List<Long> associatedIdsM2MSet = getAssociatedModelsMapForJoinTable().get(
 				associatedModelName);
 		if (associatedIdsM2MSet == null) {
-			associatedIdsM2MSet = new HashSet<Long>();
+			associatedIdsM2MSet = new ArrayList<Long>();
 			associatedIdsM2MSet.add(associatedId);
 			associatedModelsMapForJoinTable.put(associatedModelName, associatedIdsM2MSet);
 		} else {
@@ -2162,10 +2162,10 @@ public class DataSupport {
 	 *            The name of associated model.
 	 */
 	void addEmptyModelForJoinTable(String associatedModelName) {
-		Set<Long> associatedIdsM2MSet = getAssociatedModelsMapForJoinTable().get(
+		List<Long> associatedIdsM2MSet = getAssociatedModelsMapForJoinTable().get(
 				associatedModelName);
 		if (associatedIdsM2MSet == null) {
-			associatedIdsM2MSet = new HashSet<Long>();
+			associatedIdsM2MSet = new ArrayList<Long>();
 			associatedModelsMapForJoinTable.put(associatedModelName, associatedIdsM2MSet);
 		}
 	}
@@ -2178,9 +2178,9 @@ public class DataSupport {
 	 * @return An associated model's map to save values into intermediate join
 	 *         table
 	 */
-	Map<String, Set<Long>> getAssociatedModelsMapForJoinTable() {
+	Map<String, List<Long>> getAssociatedModelsMapForJoinTable() {
 		if (associatedModelsMapForJoinTable == null) {
-			associatedModelsMapForJoinTable = new HashMap<String, Set<Long>>();
+			associatedModelsMapForJoinTable = new HashMap<String, List<Long>>();
 		}
 		return associatedModelsMapForJoinTable;
 	}
--- litepal/src/main/java/org/litepal/crud/SaveHandler.java
+++ litepal/src/main/java/org/litepal/crud/SaveHandler.java
@@ -498,7 +498,7 @@ class SaveHandler extends DataHandler {
 	 *            The current action is update or not.
 	 */
 	private void insertIntermediateJoinTableValue(DataSupport baseObj, boolean isUpdate) {
-		Map<String, Set<Long>> associatedIdsM2M = baseObj.getAssociatedModelsMapForJoinTable();
+		Map<String, List<Long>> associatedIdsM2M = baseObj.getAssociatedModelsMapForJoinTable();
 		ContentValues values = new ContentValues();
 		for (String associatedTableName : associatedIdsM2M.keySet()) {
 			String joinTableName = getIntermediateTableName(baseObj, associatedTableName);
@@ -506,7 +506,7 @@ class SaveHandler extends DataHandler {
 				mDatabase.delete(joinTableName, getWhereForJoinTableToDelete(baseObj),
 						new String[] { String.valueOf(baseObj.getBaseObjId()) });
 			}
-			Set<Long> associatedIdsM2MSet = associatedIdsM2M.get(associatedTableName);
+			List<Long> associatedIdsM2MSet = associatedIdsM2M.get(associatedTableName);
 			for (long associatedId : associatedIdsM2MSet) {
 				values.clear();
 				values.put(getForeignKeyColumnName(baseObj.getTableName()), baseObj.getBaseObjId());
